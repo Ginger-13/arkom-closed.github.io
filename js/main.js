cb4f574ee6a002245ba3963705bdb593409b7584
Ginger-13 <shepardcommander13.0@gmail.com>
2017-08-27 18:28:31 +0300

    timer

---
+++ js/main.js
@@ -0,0 +1,23 @@
+(function () {
+    var $digitHour   = $('#hours'),
+        $digitMinute = $('#minutes'),
+        $digitSecond = $('#seconds');
+
+    function zeroPad (number) {
+        if (number < 10) {
+            number = "0" + number.toString();
+        }
+        return number;
+    }
+
+    setInterval(function () {
+        var now    = new Date(),
+            hour   = 23 - now.getHours(),
+            minute = 59 - now.getMinutes(),
+            second = 59 - now.getSeconds();
+
+        $digitSecond.text(zeroPad(second));
+        $digitMinute.text(zeroPad(minute));
+        $digitHour.text(zeroPad(hour));
+    }, 1000);
+})();
